3990a442b9ec976e030b8d5e4f32bac9a9e7f6e0
Glory Gweth <glorygweth6@gmail.com>
2023-08-21 21:46:34 +0300

    comment added

--- Positive.py
+++ Positive.py
@@ -1,4 +1,6 @@
 def even(a,b,c):
+    # Use of if statement to provide the diffrent situations that will return true
+    #~Only even numbers above 0 are considered to output true
     if a>0 and b>0 and a % 2 == 0 and b % 2==0 and c % 2!=0:
         print(True)
         return True
--- Timeconverter.py
+++ Timeconverter.py
@@ -2,6 +2,7 @@ def converter(hour, min, period):
     if hour < 1 or hour > 12 or min < 0 or min > 59 or (period != "am" and period != "pm"):
         print("Invalid input. Hour: 1-12, Min: 0-59, Period: am/pm")
         return "Invalid input. Hour: 1-12, Min: 0-59, Period: am/pm"
+    #Provide error message for invalid input of data
     else:
         if period == "am" and hour == 12:
             newHour = 0
@@ -11,8 +12,8 @@ def converter(hour, min, period):
             newHour = hour + 12
         else:
             newHour = hour
-    
-    time = f"{newHour:02d}{min:02d}"
+    #I f statement used to convert time depending on the period and hour.Minutes are not affected
+    time = f"{newHour:02d}{min:02d}"#ensure hour and minutes are only ouput using 2 digits each.
     print(time)
     return time
 converter(11,30,"pm")
